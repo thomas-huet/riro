1f784218666689663db48a924a9b31699fca3c4a
Thomas HUET <git@thomash.fr>
2019-03-04 12:28:49 +0100

    Generate more challenging positions

--- src/game.ml
+++ src/game.ml
@@ -11,7 +11,7 @@ let default_options = {
   height = 6;
   width = 6;
   num_walls = 4;
-  min_num_moves = 3;
+  min_num_moves = 4;
 }
 
 type cell =
@@ -35,11 +35,65 @@ let coord_of_dir = function
 
 type move = int * direction
 
-let move position m =
-  None (* TODO *)
+let move position (r, d) =
+  let height = Array.length position.grid in
+  let width = Array.length position.grid.(0) in
+  let y, x = position.robots.(r) in
+  let i, j = coord_of_dir d in
+  let is_empty a b =
+    a >= 0 && a < height && b >= 0 && b < width
+    && position.grid.(a).(b) = Empty
+  in
+  if not (is_empty (y + i) (x + j)) then
+    None
+  else begin
+    let grid = Array.map Array.copy position.grid in
+    grid.(y).(x) <- Empty;
+    let robots = Array.copy position.robots in
+    let rec find_stop m =
+      if is_empty (y + m * i) (x + m * j) then
+        find_stop (m + 1)
+      else
+        y + (m - 1) * i, x + (m - 1) * j
+    in
+    let y, x = find_stop 2 in
+    robots.(r) <- y, x;
+    grid.(y).(x) <- Robot r;
+    Some { position with grid; robots }
+  end
+
+let winning position =
+  position.robots.(0) = position.target
 
 let find_solution position depth =
-  None (* TODO *)
+  let q = Queue.create () in
+  let h = Hashtbl.create 5 in
+  Queue.push (depth, position) q;
+  Hashtbl.add h position ();
+  let rec explore () =
+    if Queue.is_empty q then None else
+    let depth, position = Queue.pop q in
+    if winning position then
+      Some ()
+    else begin
+      if depth > 0 then begin
+        let try_move m = match move position m with
+        | None -> ()
+        | Some position ->
+          try Hashtbl.find h position with
+          Not_found -> begin
+            Queue.push (depth - 1, position) q;
+            Hashtbl.add h position ();
+          end
+        in
+        for i = 0 to Array.length position.robots - 1 do
+          List.iter (fun d -> try_move (i, d)) [Up; Down; Left; Right]
+        done
+      end;
+      explore ()
+    end
+  in
+  explore ()
 
 let exists_path position =
   true (* TODO *)
