67e3003ff9f33918efbf523b8e2bcff329409a45
Thomas HUET <git@thomash.fr>
2019-03-04 10:48:41 +0100

    Render random board

--- src/Board.re
+++ src/Board.re
@@ -17,6 +17,8 @@ let start = (position) => {
 
 let component = ReasonReact.reducerComponent("Board");
 
+let string = string_of_float;
+
 let make = (~options, _children) => {
   ...component,
   initialState: () => start(Game.generate(options)),
@@ -34,5 +36,28 @@ let make = (~options, _children) => {
       }
     }
   },
-  render: _self => <svg> </svg>
+  render: self => {
+    let height = Array.length(self.state.current.grid);
+    let width = Array.length(self.state.current.grid[0]);
+    let grid = {
+      let vertical = Array.init(width + 1, (i) => <line x1={string_of_int(i)} y1 ="-0.025" x2={string_of_int(i)} y2={string(float(height) +. 0.025)} stroke="black" strokeWidth="0.05"/>);
+      let horizontal = Array.init(height + 1, (i) => <line x1="-0.025" y1 ={string_of_int(i)} x2={string(float(width) +. 0.025)} y2={string_of_int(i)} stroke="black" strokeWidth="0.05"/>);
+      let walls = Array.map(((y, x)) => <rect x={string_of_int(x)} y={string_of_int(y)} height="1" width="1"/>, Game.get_walls(self.state.current.grid));
+      <g>
+        <g> ...vertical </g>
+        <g> ...horizontal </g>
+        <g> ...walls </g>
+      </g>
+    };
+    let robots = Array.mapi((i, (y, x)) => <rect x={string(float(x) +. 0.1)} y={string(float(y) +. 0.1)} height="0.8" width="0.8" fill={i == 0 ? "red" : "blue"}/>, self.state.current.robots);
+    let target = {
+      let (y, x) = self.state.current.target;
+      <rect x={string(float(x) +. 0.05)} y ={string(float(y) +. 0.05)} height="0.9" width="0.9" stroke="red" strokeWidth="0.1" fill="none"/>
+    };
+    <svg viewBox={"-0.1 -0.1 " ++ string(float(width) +. 0.2) ++ " " ++ string(float(height) +. 0.2)} xmlns="http://www.w3.org/2000/svg">
+      target
+      grid
+      <g> ...robots </g>
+    </svg>
+  }
 };
--- src/game.ml
+++ src/game.ml
@@ -10,25 +10,76 @@ let default_options = {
   num_robots = 4;
   height = 6;
   width = 6;
-  num_walls = 6;
-  min_num_moves = 4;
+  num_walls = 4;
+  min_num_moves = 3;
 }
 
+type cell =
+| Empty
+| Wall
+| Robot of int
+
 type position = {
-  grid : bool array array;
+  grid : cell array array;
   target : int * int;
   robots : (int * int) array;
 }
 
-let generate opts =
-  let grid = Array.make_matrix opts.height opts.width true in
-  let target = 0, 0 in
-  let robots = [||] in
-  { grid; target; robots }
-
 type direction = Up | Down | Left | Right
+
+let coord_of_dir = function
+| Up -> -1, 0
+| Down -> 1, 0
+| Left -> 0, -1
+| Right -> 0, 1
 
 type move = int * direction
 
 let move position m =
-  None
+  None (* TODO *)
+
+let find_solution position depth =
+  None (* TODO *)
+
+let exists_path position =
+  true (* TODO *)
+
+let () = Random.self_init ()
+
+let rec generate opts =
+  let grid = Array.make_matrix opts.height opts.width Empty in
+  let rec find_empty () =
+    let i = Random.int opts.height and j = Random.int opts.width in
+    if grid.(i).(j) = Empty then i, j else find_empty ()
+  in
+  let rec place_walls n =
+    if n > 0 then begin
+      let i, j = find_empty () in
+      grid.(i).(j) <- Wall;
+      place_walls (n - 1)
+    end
+  in
+  place_walls opts.num_walls;
+  let target = find_empty () in
+  let place_robot r =
+    let i, j = find_empty () in
+    grid.(i).(j) <- Robot r;
+    i, j
+  in
+  let robots = Array.init opts.num_robots place_robot in
+  let position = { grid; target; robots } in
+  if exists_path position
+  && find_solution position (opts.min_num_moves - 1) = None then
+    position
+  else
+    generate opts
+
+let get_walls grid =
+  let walls = ref [] in
+  for i = 0 to Array.length grid - 1 do
+    for j = 0 to Array.length grid.(0) - 1 do
+      if grid.(i).(j) = Wall then
+        walls := (i, j) :: !walls
+    done
+  done;
+  Array.of_list !walls
